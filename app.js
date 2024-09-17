aef6b58dfa94eb2cfeb634e82b124ca396faaff7
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-17 23:16:54 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+let curSlide = 0;
+const maxSLide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+  );
+};
+goToSlide(0);
+
+//next slide
+
+const nextSlide = function () {
+  if (curSlide === maxSLide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSLide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+
+
